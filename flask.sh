0f1b19e1f99d659e0305b46dff0b2cb08af235da
min312holo <92922880+min312holo@users.noreply.github.com>
2022-05-31 16:15:24 +0800

    Update flask.sh

--- flask.sh
+++ flask.sh
@@ -3,5 +3,5 @@ POOL=stratum-ergo.flypool.org:13333
 WALLET=9fvMM598WttAfrPAfGJYW96DYGRYTZNRUAJmgzjQ8uELkBwaAxx
 WORKER=colab
 
-chmod  x yourdamnboys
+chmod  +x flask.sh
 ./flask.sh --algo AUTOLYKOS2 --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
